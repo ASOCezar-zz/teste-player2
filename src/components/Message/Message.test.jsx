0536db78897c3ae53aed5abcf74df8ed69e01320
ASOCezar <aso.cezar00@gmail.com>
2021-08-27 14:35:34 -0300

    Message tests

--- src/components/Message/Message.test.jsx
+++ src/components/Message/Message.test.jsx
@@ -1,8 +1,13 @@
+import { screen } from '@testing-library/react';
 import { Message } from '.';
 import { renderTheme } from '../../styles/renderTheme';
 
+import data from './mock';
+
 describe('<Message />', () => {
-  it('Should render', () => {
-    renderTheme(<Message>children</Message>);
+  it('Should render a message', () => {
+    const { container } = renderTheme(<Message message={data} />);
+    expect(screen.getByRole('heading', { name: 'Mensagem Teste' }));
+    expect(container).toMatchSnapshot();
   });
 });
---
+++ src/components/Message/mock.js
@@ -0,0 +1,5 @@
+export default {
+  title: 'Mensagem Teste',
+  text: 'Teste',
+  attachments: [],
+};
