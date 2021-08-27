36401645812866b622967639700ef75dd506a374
ASOCezar <aso.cezar00@gmail.com>
2021-08-27 15:05:48 -0300

    NavigationLink tests

--- src/components/NavigationLink/NavigationLink.test.jsx
+++ src/components/NavigationLink/NavigationLink.test.jsx
@@ -1,8 +1,20 @@
+import userEvent from '@testing-library/user-event';
 import { NavigationLink } from '.';
 import { renderTheme } from '../../styles/renderTheme';
 
+const fn = jest.fn();
+const fn2 = jest.fn();
+
 describe('<NavigationLink />', () => {
-  it('Should render', () => {
-    renderTheme(<NavigationLink>children</NavigationLink>);
+  it('Should render a navigation link and observes mouse events', () => {
+    const { container } = renderTheme(<NavigationLink onMouseOver={fn} onMouseLeave={fn2} />);
+
+    userEvent.hover(container.firstChild);
+    expect(fn).toBeCalled();
+
+    userEvent.unhover(container.firstChild);
+    expect(fn2).toBeCalled();
+
+    expect(container).toMatchSnapshot();
   });
 });
