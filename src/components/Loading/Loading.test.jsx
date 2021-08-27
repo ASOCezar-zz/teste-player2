ed541ec93c9b289dc072407d1b3595960339b501
ASOCezar <aso.cezar00@gmail.com>
2021-08-27 14:30:04 -0300

    Loadingg tests

--- src/components/Loading/Loading.test.jsx
+++ src/components/Loading/Loading.test.jsx
@@ -2,7 +2,8 @@ import { Loading } from '.';
 import { renderTheme } from '../../styles/renderTheme';
 
 describe('<Loading />', () => {
-  it('Should render', () => {
-    renderTheme(<Loading>children</Loading>);
+  it('Should render a loading animation', () => {
+    const { container } = renderTheme(<Loading />);
+    expect(container).toMatchSnapshot();
   });
 });
